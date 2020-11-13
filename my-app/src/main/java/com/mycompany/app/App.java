209f7b49774ac4b2f702506ee395282ff1140203
arithmetic1728 <liusijun1985@gmail.com>
2020-11-12 22:20:01 -0800

    update test

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -5,20 +5,18 @@ import java.security.GeneralSecurityException;
 import java.util.Collections;
 
 import com.google.api.client.http.HttpRequestInitializer;
-import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.http.HttpTransport;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.services.cloudkms.v1.CloudKMS;
 import com.google.api.services.cloudkms.v1.CloudKMS.Projects.Locations.KeyRings;
 import com.google.api.services.cloudkms.v1.model.ListKeyRingsResponse;
 import com.google.auth.http.HttpCredentialsAdapter;
 import com.google.auth.oauth2.GoogleCredentials;
+
 import com.google.api.services.cloudkms.v1.CloudKMSScopes;
-import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 
 public class App {
-  private static CloudKMS createClient() throws GeneralSecurityException, IOException {
-    NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
-
+  public static CloudKMS createClient(HttpTransport transport) throws GeneralSecurityException, IOException, Exception {
     GoogleCredentials credential = GoogleCredentials.getApplicationDefault()
         .createScoped(Collections.singleton(CloudKMSScopes.CLOUD_PLATFORM));
 
@@ -31,13 +29,13 @@ public class App {
         };
 
     // Build the client for interacting with the service.
-    return new CloudKMS.Builder(HTTP_TRANSPORT, new JacksonFactory(), requestInitializer)
+    return new CloudKMS.Builder(transport, new JacksonFactory(), requestInitializer)
         .setApplicationName("my application").build();
   }
 
-  public static void KeyringList(String projectId, String regionId) throws Exception {
+  public static void KeyringList(HttpTransport transport) throws Exception {
     // Initialize the client, which will be used to interact with the service.
-    CloudKMS client = createClient();
+    CloudKMS client = createClient(transport);
 
     // Results are paginated, so multiple queries may be required.
     String pageToken = null;
@@ -57,10 +55,5 @@ public class App {
       pageToken = response.getNextPageToken();
       System.out.println(pageToken);
     } while (pageToken != null);
-  }
-
-  public static void main(String[] args) throws Exception {
-    String project = "shinfan-mtls-demo";
-    App.KeyringList(project, "us-central1");
   }
 }
--- my-app/src/test/java/com/mycompany/app/AppTest.java
+++ my-app/src/test/java/com/mycompany/app/AppTest.java
@@ -2,19 +2,36 @@ package com.mycompany.app;
 
 import static org.junit.Assert.assertTrue;
 
+import com.google.api.client.googleapis.apache.v2.GoogleApacheHttpTransport;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.apache.v2.ApacheHttpTransport;
+
+import org.apache.http.client.HttpClient;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
+  // @Test
+  // public void javaNetTest() throws Exception {
+  // HttpTransport transport = GoogleNetHttpTransport.newTrustedTransport();
+  // App.KeyringList(transport);
+  // }
+
+  // @Test
+  // public void apacheTest() throws Exception
+  // {
+  // HttpTransport transport = GoogleApacheHttpTransport.newTrustedTransport();
+  // App.KeyringList(transport);
+  // }
+
+  @Test
+  public void apacheNewTest() throws Exception {
+    HttpClient client = HttpClientBuilder.create().useSystemProperties()
+        .setSSLSocketFactory(SSLConnectionSocketFactory.getSystemSocketFactory()).build();
+    HttpTransport transport = new ApacheHttpTransport(client, true);
+    App.KeyringList(transport);
+  }
 }
