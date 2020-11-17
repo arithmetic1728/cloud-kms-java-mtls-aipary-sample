423f0142fca86065de87c7bcf0e23a95d5a3cae3
arithmetic1728 <liusijun1985@gmail.com>
2020-11-16 16:45:36 -0800

    update

--- my-app/src/test/java/com/mycompany/app/AppTest.java
+++ my-app/src/test/java/com/mycompany/app/AppTest.java
@@ -1,8 +1,20 @@
 package com.mycompany.app;
 
+import java.security.KeyStore;
+
+import javax.net.ssl.SSLContext;
+
+import com.google.api.client.googleapis.GoogleUtils;
 import com.google.api.client.googleapis.apache.v2.GoogleApacheHttpTransport;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.googleapis.mtls.MtlsUtils;
 import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.apache.v2.ApacheHttpTransport;
+import com.google.api.client.util.SslUtils;
+
+import org.apache.http.conn.socket.LayeredConnectionSocketFactory;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.junit.Test;
 
 public class AppTest 
